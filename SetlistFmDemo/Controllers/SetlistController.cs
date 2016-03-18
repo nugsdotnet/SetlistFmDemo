8da78349bc788db3ff6d860e97812f2203d06c10
Shaun Linton <sinton@arke.com>
2016-03-18 12:13:36 -0400

    Initial check-in

---
+++ SetlistFmDemo/Controllers/SetlistController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SetlistFmDemo.Controllers
+{
+    public class SetlistController : ApiController
+    {
+        [HttpGet]
+        [Route("setlists")]
+        public async Task<object[]> GetPage(string p)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://api.setlist.fm/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = new HttpResponseMessage();
+                object obj;
+
+                object[] list = new object[1];
+
+                response = await client.GetAsync("rest/0.1/search/setlists.json?artistName=phish&p=" + p);
+                obj = await response.Content.ReadAsAsync<object>();
+
+                list[0] = obj;
+
+                return list;
+            }
+        }
+    }
+}
---
+++ SetlistFmDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SetlistFmDemo.WebApiApplication" Language="C#" %>
